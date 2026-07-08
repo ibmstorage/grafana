1a6121a839b4bb524cf0c32c8607b2290dc0eb21
Rakshitha Kamath <Rakshitha.Kamath@ibm.com>
2026-07-08 11:24:19 +0530

    Prebuild js files
    Signed-off-by: Rakshitha Kamath <Rakshitha.Kamath@ibm.com>

---
+++ public/app/plugins/datasource/elasticsearch/node_modules/@grafana/aws-sdk/dist/esm/types.js
@@ -0,0 +1,12 @@
+var AwsAuthType = /* @__PURE__ */ ((AwsAuthType2) => {
+  AwsAuthType2["Keys"] = "keys";
+  AwsAuthType2["Credentials"] = "credentials";
+  AwsAuthType2["Default"] = "default";
+  AwsAuthType2["EC2IAMRole"] = "ec2_iam_role";
+  AwsAuthType2["ARN"] = "arn";
+  AwsAuthType2["GrafanaAssumeRole"] = "grafana_assume_role";
+  return AwsAuthType2;
+})(AwsAuthType || {});
+
+export { AwsAuthType };
+//# sourceMappingURL=types.js.map
